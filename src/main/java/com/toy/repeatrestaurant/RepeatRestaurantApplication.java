8c344461406b8bd19d12c002ffa9ae46e5946fc1
이주영 <wruomma2@naver.com>
2024-04-08 15:05:07 +0900

    :tada: feat: 프로젝트 생성

---
+++ src/main/java/com/toy/repeatrestaurant/RepeatRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.toy.repeatrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepeatRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/repeatrestaurant/RepeatRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.repeatrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
